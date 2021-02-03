cb6fbea3d4fc985aa56bee359fe3a63feacbcd50
jtreeves <jackson.t.reeves@gmail.com>
2021-02-03 18:52:36 -0500

    [GitHub] Grab commit id's from recent posts

--- src/components/elements/GitHub.js
+++ src/components/elements/GitHub.js
@@ -1,7 +1,29 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 function GitHub() {
+    const [commits, setCommits] = useState([])
+
+    async function gettingInfo() {
+        const result = await axios.get('https://api.github.com/users/jtreeves/events')
+        const resultData = result.data
+        const resultDataArray = resultData.map((commit, index) => {
+            return (
+                <div key={index}>
+                    {commit.id}
+                </div>
+            )
+        })
+        setCommits(resultDataArray)
+    }
+
+    useEffect(() => {
+        gettingInfo()
+    }, [])
+    
     return (
         <div>
-            A feed from GitHub.
+            {commits}
         </div>
     )
 }
--- src/components/pages/Connect.js
+++ src/components/pages/Connect.js
@@ -1,8 +1,11 @@
+import GitHub from '../elements/GitHub'
+
 function Connect() {
     return (
         <div>
             <h2>Connect</h2>
             <p>Get in touch with me on social media.</p>
+            <GitHub />
         </div>
     )
 }
