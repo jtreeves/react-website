6614973ac0f980e90945ef0a281f6915d9a248f6
jtreeves <jackson.t.reeves@gmail.com>
2021-02-03 20:17:27 -0500

    [Twitter] Grab recent tweets

--- src/components/elements/Twitter.js
+++ src/components/elements/Twitter.js
@@ -1,7 +1,39 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
+const CONSUMER_KEY = process.env.TWITTER_OAUTH_CONSUMER_KEY
+const TOKEN = process.env.TWITTER_OAUTH_TOKEN
+const twitterUrl = 'https://api.twitter.com/2/users/15182079/tweets'
+const twitterHeaders = { 
+    'Authorization': 'OAuth oauth_consumer_key="' + CONSUMER_KEY + '",oauth_token="' + TOKEN + '",oauth_signature_method="HMAC-SHA1"'
+}
+
 function Twitter() {
+    const [tweets, setTweets] = useState([])
+    
+    async function gettingTwitter() {
+        const result = await axios.get({
+            url: twitterUrl,
+            headers: twitterHeaders
+        })
+        const resultData = result.data
+        const resultDataArray = resultData.map((tweet, index) => {
+            return (
+                <div key={index}>
+                    {tweet.text}
+                </div>
+            )
+        })
+        setTweets(resultDataArray)
+    }
+
+    useEffect(() => {
+        gettingTwitter()
+    }, [])
+    
     return (
         <div>
-            A feed from Twitter.
+            {tweets}
         </div>
     )
 }
