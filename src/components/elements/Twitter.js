7e573b871e5cfcc92786153023ab1703640a2c59
jtreeves <jackson.t.reeves@gmail.com>
2021-02-03 23:39:52 -0500

    [Twitter] Refactor to receive information from server repo

--- src/components/elements/Twitter.js
+++ src/components/elements/Twitter.js
@@ -1,25 +1,24 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 
-const CONSUMER_KEY = process.env.REACT_APP_TWITTER_OAUTH_CONSUMER_KEY
-const TOKEN = process.env.REACT_APP_TWITTER_OAUTH_TOKEN
-const twitterUrl = 'https://api.twitter.com/2/users/15182079/tweets'
-const twitterHeaders = { 
-    'Authorization': 'OAuth oauth_consumer_key="' + CONSUMER_KEY + '",oauth_token="' + TOKEN + '",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1612399605",oauth_nonce="vwqDpKzb1OI",oauth_version="1.0",oauth_signature="W94Un693iVA%2FV4KVTpubukHwlW0%3D"',
-    'Cookie': 'personalization_id="v1_ZTziNz8G674lqjpwGOyjsQ=="; guest_id=v1%3A161239937142054139'
-}
+// const CONSUMER_KEY = process.env.REACT_APP_TWITTER_OAUTH_CONSUMER_KEY
+// const TOKEN = process.env.REACT_APP_TWITTER_OAUTH_TOKEN
+// const twitterUrl = 'https://api.twitter.com/2/users/15182079/tweets'
+// const twitterHeaders = { 
+//     'Authorization': 'OAuth oauth_consumer_key="' + CONSUMER_KEY + '",oauth_token="' + TOKEN + '",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1612399605",oauth_nonce="vwqDpKzb1OI",oauth_version="1.0",oauth_signature="W94Un693iVA%2FV4KVTpubukHwlW0%3D"',
+//     'Cookie': 'personalization_id="v1_ZTziNz8G674lqjpwGOyjsQ=="; guest_id=v1%3A161239937142054139'
+// }
+
+const appServer = process.env.REACT_APP_SERVER_URL
 
 function Twitter() {
     const [tweets, setTweets] = useState([])
     
     async function gettingTwitter() {
-        console.log(`TWITTERURL: ${twitterUrl}`)
-        console.log(`TWITTERHEADERS: ${twitterHeaders}`)
-        console.log(`TWITTERHEADERS KEYS: ${Object.keys(twitterHeaders)}`)
-        console.log(`TWITTERHEADERS.AUTHORIZATION: ${twitterHeaders.Authorization}`)
         try {
-            const result = await axios.get(twitterUrl, { twitterHeaders })
-            console.log(`RESULT: ${result}`)
+            const result = await axios.get(
+                appServer + '/twitter'
+            )
             const resultData = result.data
             console.log(`RESULT.DATA: ${resultData}`)
             const resultDataArray = resultData.map((tweet, index) => {
