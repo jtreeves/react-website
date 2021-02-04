5599a12e43588735dd95898592f02124075d1509
jtreeves <jackson.t.reeves@gmail.com>
2021-02-04 11:52:18 -0500

    [Tumblr] Build out function to receive data from backend

--- src/components/elements/Tumblr.js
+++ src/components/elements/Tumblr.js
@@ -1,7 +1,37 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const appServer = process.env.REACT_APP_SERVER_URL
+
 function Tumblr() {
+    const [posts, setPosts] = useState([])
+
+    async function gettingInfo() {
+        try {
+            const result = await axios.get(
+                appServer + '/tumblr'
+            )
+            const resultData = result.data
+            const resultDataArray = resultData.commits.map((post, index) => {
+                return (
+                    <div key={index}>
+                        {post.title}
+                    </div>
+                )
+            })
+            setPosts(resultDataArray)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        gettingInfo()
+    }, [])
+    
     return (
         <div>
-            A feed from Tumblr.
+            {posts}
         </div>
     )
 }
