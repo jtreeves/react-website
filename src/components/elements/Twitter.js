40fd4d08b693bcc903e96f25ae54575fb39fb950
jtreeves <jackson.t.reeves@gmail.com>
2021-02-03 21:28:37 -0500

    [Twitter] Wrap in try/catch blocks to display error

--- src/components/elements/Twitter.js
+++ src/components/elements/Twitter.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 
-const CONSUMER_KEY = process.env.TWITTER_OAUTH_CONSUMER_KEY
-const TOKEN = process.env.TWITTER_OAUTH_TOKEN
+const CONSUMER_KEY = process.env.REACT_APP_TWITTER_OAUTH_CONSUMER_KEY
+const TOKEN = process.env.REACT_APP_TWITTER_OAUTH_TOKEN
 const twitterUrl = 'https://api.twitter.com/2/users/15182079/tweets'
 const twitterHeaders = { 
     'Authorization': 'OAuth oauth_consumer_key="' + CONSUMER_KEY + '",oauth_token="' + TOKEN + '",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1612399605",oauth_nonce="vwqDpKzb1OI",oauth_version="1.0",oauth_signature="W94Un693iVA%2FV4KVTpubukHwlW0%3D"',
@@ -15,18 +15,24 @@ function Twitter() {
     async function gettingTwitter() {
         console.log(`TWITTERURL: ${twitterUrl}`)
         console.log(`TWITTERHEADERS: ${twitterHeaders}`)
-        const result = await axios.get(twitterUrl)
-        console.log(`RESULT: ${result}`)
-        const resultData = result.data
-        console.log(`RESULT.DATA: ${resultData}`)
-        const resultDataArray = resultData.map((tweet, index) => {
-            return (
-                <div key={index}>
-                    {tweet.text}
-                </div>
-            )
-        })
-        setTweets(resultDataArray)
+        console.log(`TWITTERHEADERS KEYS: ${Object.keys(twitterHeaders)}`)
+        console.log(`TWITTERHEADERS.AUTHORIZATION: ${twitterHeaders.Authorization}`)
+        try {
+            const result = await axios.get(twitterUrl, { twitterHeaders })
+            console.log(`RESULT: ${result}`)
+            const resultData = result.data
+            console.log(`RESULT.DATA: ${resultData}`)
+            const resultDataArray = resultData.map((tweet, index) => {
+                return (
+                    <div key={index}>
+                        {tweet.text}
+                    </div>
+                )
+            })
+            setTweets(resultDataArray)
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     useEffect(() => {
