0553a62d447dda8a08ee85ce0f445ff6533001ef
jtreeves <jackson.t.reeves@gmail.com>
2021-02-05 16:43:08 -0500

    [contents] Reorganize import statements at top of files

--- src/components/contents/Twitter.js
+++ src/components/contents/Twitter.js
@@ -1,5 +1,5 @@
-import axios from 'axios'
 import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const appServer = process.env.REACT_APP_SERVER_URL
 const link = 'https://twitter.com/JTReeves/status/'
