180c67d1a28968ed69caa9ffb7598016be6b2b57
jtreeves <jackson.t.reeves@gmail.com>
2021-02-05 15:46:40 -0500

    [Portfolio] Add Kaleidoscope entry using a Project

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,14 +1,30 @@
 import Introduction from '../sections/Introduction'
+import Project from '../sections/Project'
 
 function Portfolio() {
     const name = 'Portfolio'
     const description = 'Here are some of my recent projects.'
+
+    const firstProject = {
+        name: 'Kaleidoscope',
+        description: 'A budget app',
+        image: '/images/kaleidoscope.png',
+        deployedLink: 'https://kaleidoscope-budget.herokuapp.com',
+        repositoryLink: 'https://github.com/jtreeves/budget-frontend'
+    }
 
     return (
         <div>
             <Introduction 
                 name={name}
                 description={description}
+            />
+            <Project
+                name={firstProject.name}
+                description={firstProject.description}
+                image={firstProject.image}
+                deployedLink={firstProject.deployedLink}
+                repositoryLink={firstProject.repositoryLink}
             />
         </div>
     )
---
+++ src/components/sections/Project.js
@@ -0,0 +1,28 @@
+function Project(props) {
+    return (
+        <div>
+            <h3>{props.name}</h3>
+            <p>{props.description}</p>
+            <img src={props.image} alt={props.name} />
+            <p>
+                <a 
+                    href={props.deployedLink} 
+                    target="_blank" 
+                    rel="noreferrer"
+                >
+                    <strong>App</strong>
+                </a>
+                &nbsp;|&nbsp;
+                <a 
+                    href={props.repositoryLink} 
+                    target="_blank" 
+                    rel="noreferrer"
+                >
+                    <strong>Code</strong>
+                </a>
+            </p>
+        </div>
+    )
+}
+
+export default Project
