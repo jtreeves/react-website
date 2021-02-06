4f79f10c1cbf5ab6ccb8b91adb27d54794809da5
jtreeves <jackson.t.reeves@gmail.com>
2021-02-06 18:54:16 -0500

    [css] Remove bullets from navigation bar by setting list-style to 'none'; center list elements by setting margin and padding to 0

--- src/components/sections/Navigation.js
+++ src/components/sections/Navigation.js
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom'
 
 function Navigation() {
     return (
-        <ul>
+        <ul className="navigation">
             <li>
                 <NavLink exact to="/about">
                     About
