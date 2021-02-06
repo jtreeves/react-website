2a1a117d713d260062e223a631996e93eb78d5ad
jtreeves <jackson.t.reeves@gmail.com>
2021-02-06 15:15:58 -0500

    [Skill] Render a Project component for each project associated with a specific skill

--- src/components/pages/Skill.js
+++ src/components/pages/Skill.js
@@ -1,6 +1,7 @@
 import { useLocation } from 'react-router-dom'
 
 import Introduction from '../sections/Introduction'
+import Project from '../sections/Project'
 
 import skills from '../../database/skills'
 import projects from '../../database/projects'
@@ -11,6 +12,22 @@ function Skill() {
     const name = 'Skill'
     const description = `Here are some projects I have built using ${specificSkill}.`
 
+    const projectList = skills[specificSkill].map((project, index) => {
+        return (
+            <div key={index}>
+                <Project
+                    name={projects[project].name}
+                    description={projects[project].description}
+                    skills={projects[project].skills}
+                    type={projects[project].type}
+                    image={projects[project].image}
+                    deployedLink={projects[project].deployedLink}
+                    repositoryLink={projects[project].repositoryLink}
+                />
+            </div>
+        )
+    })
+
     return (
         <div>
             <Introduction 
@@ -18,7 +35,7 @@ function Skill() {
                 description={description}
             />
 
-            {skills[specificSkill]}
+            {projectList}
         </div>
     )
 }
