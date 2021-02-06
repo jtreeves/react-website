1fd65722892d2d6ea6874bed12f2e94db791c70d
jtreeves <jackson.t.reeves@gmail.com>
2021-02-06 15:51:42 -0500

    [skills] Add line breaks and quotes for clarity and consistency

--- src/database/skills.js
+++ src/database/skills.js
@@ -1,28 +1,114 @@
 const skills = {
-    'Agile Development': ['kaleidoscope'],
-    Bootstrap: ['readThisNext'],
-    'Command Line': ['regressions', 'kaleidoscope', 'tiresias', 'matchesAndPatches', 'readThisNext'],
-    CSS: ['matchesAndPatches'],
-    Express: ['kaleidoscope', 'tiresias', 'readThisNext'],
-    Git: ['regressions', 'kaleidoscope', 'tiresias', 'matchesAndPatches', 'readThisNext'],
-    GitHub: ['regressions', 'kaleidoscope', 'tiresias', 'matchesAndPatches', 'readThisNext'],
-    Heroku: ['kaleidoscope', 'tiresias', 'readThisNext'],
-    HTML: ['matchesAndPatches'],
-    JavaScript: ['kaleidoscope', 'tiresias', 'matchesAndPatches', 'readThisNext'],
-    MongoDB: ['kaleidoscope', 'tiresias'],
-    Mongoose: ['kaleidoscope', 'tiresias'],
-    'MVC Pattern': ['kaleidoscope', 'tiresias', 'readThisNext'],
-    Node: ['kaleidoscope', 'tiresias', 'readThisNext'],
-    'Object-Oriented Programming': ['regressions', 'kaleidoscope', 'tiresias', 'matchesAndPatches', 'readThisNext'],
-    Postgres: ['readThisNext'],
-    Python: ['regressions'],
-    PyPi: ['regressions'],
-    React: ['kaleidoscope', 'tiresias'],
-    'Responsive Design': ['kaleidoscope'],
-    Sequelize: ['readThisNext'],
-    SQL: ['readThisNext'],
-    'User Stories': ['regressions', 'kaleidoscope', 'tiresias', 'matchesAndPatches', 'readThisNext'],
-    Wireframing: ['regressions', 'kaleidoscope', 'tiresias', 'matchesAndPatches', 'readThisNext']
+    'Agile Development': [
+        'kaleidoscope'
+    ],
+    'Bootstrap': [
+        'readThisNext'
+    ],
+    'Command Line': [
+        'regressions', 
+        'kaleidoscope', 
+        'tiresias', 
+        'matchesAndPatches', 
+        'readThisNext'
+    ],
+    'CSS': [
+        'matchesAndPatches'
+    ],
+    'Express': [
+        'kaleidoscope', 
+        'tiresias', 
+        'readThisNext'
+    ],
+    'Git': [
+        'regressions', 
+        'kaleidoscope', 
+        'tiresias', 
+        'matchesAndPatches', 
+        'readThisNext'
+    ],
+    'GitHub': [
+        'regressions', 
+        'kaleidoscope', 
+        'tiresias', 
+        'matchesAndPatches', 
+        'readThisNext'
+    ],
+    'Heroku': [
+        'kaleidoscope', 
+        'tiresias', 
+        'readThisNext'
+    ],
+    'HTML': [
+        'matchesAndPatches'
+    ],
+    'JavaScript': [
+        'kaleidoscope', 
+        'tiresias', 
+        'matchesAndPatches', 
+        'readThisNext'
+    ],
+    'MongoDB': [
+        'kaleidoscope', 
+        'tiresias'
+    ],
+    'Mongoose': [
+        'kaleidoscope', 
+        'tiresias'
+    ],
+    'MVC Pattern': [
+        'kaleidoscope', 
+        'tiresias', 
+        'readThisNext'
+    ],
+    'Node': [
+        'kaleidoscope', 
+        'tiresias', 
+        'readThisNext'
+    ],
+    'Object-Oriented Programming': [
+        'regressions', 
+        'kaleidoscope', 
+        'tiresias', 
+        'matchesAndPatches', 
+        'readThisNext'
+    ],
+    'Postgres': [
+        'readThisNext'
+    ],
+    'Python': [
+        'regressions'
+    ],
+    'PyPi': [
+        'regressions'
+    ],
+    'React': [
+        'kaleidoscope', 
+        'tiresias'
+    ],
+    'Responsive Design': [
+        'kaleidoscope'
+    ],
+    'Sequelize': [
+        'readThisNext'
+    ],
+    'SQL': [
+        'readThisNext'
+    ],
+    'User Stories': [
+        'regressions', 
+        'kaleidoscope', 
+        'tiresias', 
+        'matchesAndPatches', 
+        'readThisNext'
+    ],
+    'Wireframing': [
+        'regressions', 
+        'kaleidoscope', 
+        'tiresias', 
+        'matchesAndPatches', 
+        'readThisNext'
+    ]
 }
 
 export default skills
