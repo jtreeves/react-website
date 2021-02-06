821e8abfd19c8db5648f989a4633b3a5622cb52c
jtreeves <jackson.t.reeves@gmail.com>
2021-02-06 00:36:51 -0500

    [Resume] Add multiple skill sets

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,10 +1,52 @@
+import Education from '../sections/Education'
 import Introduction from '../sections/Introduction'
+import Job from '../sections/Job'
 import SkillSet from '../sections/SkillSet'
 
 function Resume() {
     const name = 'Resume'
     const description = 'Here is my current resume.'
-    const skills = ['JavaScript', 'Python', 'Ruby']
+    
+    const languages = [
+        'JavaScript', 
+        'Python', 
+        'Ruby', 
+        'HTML', 
+        'CSS'
+    ]
+
+    const frameworks = [
+        'Node', 
+        'React', 
+        'Express', 
+        'Django', 
+        'Flask', 
+        'Bootstrap'
+    ]
+
+    const management = [
+        'Git', 
+        'GitHub', 
+        'Heroku',
+        'Command Line'
+    ]
+
+    const databases = [
+        'SQL',
+        'Postgres', 
+        'Sequelize', 
+        'MongoDB',
+        'Mongoose'
+    ]
+
+    const methodologies = [
+        'Object-Oriented Programming',
+        'Agile Development', 
+        'MVC Pattern', 
+        'Responsive Design', 
+        'User Stories', 
+        'Wireframing'
+    ]
 
     return (
         <div>
@@ -13,9 +55,44 @@ function Resume() {
                 description={description}
             />
 
-            <SkillSet
-                skills={skills}
-            />
+            <div>
+                <strong>Languages</strong>
+                <SkillSet
+                    skills={languages}
+                />
+            </div>
+            
+            <div>
+                <strong>Frameworks</strong>
+                <SkillSet
+                    skills={frameworks}
+                />
+            </div>
+            
+            <div>
+                <strong>Management</strong>
+                <SkillSet
+                    skills={management}
+                />
+            </div>
+
+            <div>
+                <strong>Databases</strong>
+                <SkillSet
+                    skills={databases}
+                />
+            </div>
+
+            <div>
+                <strong>Methodologies</strong>
+                <SkillSet
+                    skills={methodologies}
+                />
+            </div>
+
+            {/* <Job />
+
+            <Education /> */}
         </div>
     )
 }
