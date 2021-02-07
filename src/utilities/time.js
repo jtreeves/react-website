c2c5cb703ad8d7aedcde408f5e00623d92469039
jtreeves <jackson.t.reeves@gmail.com>
2021-02-06 23:59:33 -0500

    [time] Refactor for clarity and organization

--- src/utilities/time.js
+++ src/utilities/time.js
@@ -1,13 +1,11 @@
 function convertTime(date) {
     const year = parseInt(date.slice(0, 4))
     const month = parseInt(date.slice(5, 7))
-    console.log(`MONTH: ${month}`)
     const day = parseInt(date.slice(8, 10))
     const hour = parseInt(date.slice(11, 13))
-    const minute = date.slice(14, 16)
-    const minuteInt = parseInt(minute)
+    const minute = parseInt(date.slice(14, 16))
     const second = parseInt(date.slice(17, 19))
-    const time = second + 60 * minuteInt + 3600 * hour
+    const time = second + 60 * minute + 3600 * hour
     const convertTime = time - 3600 * 5
     let correctTime = null
     let correctDay = null     
@@ -18,80 +16,83 @@ function convertTime(date) {
         correctTime = convertTime
         correctDay = day
     }
-    const correctHour = Math.floor(correctTime / 3600)
-    let meridiemHour = null
-    if (correctHour > 12) {
-        meridiemHour = correctHour - 12
-    } else if (correctHour === 0) {
-        meridiemHour = 12
+    let finalMinute = null
+    if (minute < 10) {
+        finalMinute = '0' + minute.toString()
     } else {
-        meridiemHour = correctHour
+        finalMinute = minute
     }
-    let lettersForHours = ''
+    const correctHour = Math.floor(correctTime / 3600)
+    let finalHour = null
+    if (correctHour > 12) {
+        finalHour = correctHour - 12
+    } else if (correctHour === 0) {
+        finalHour = 12
+    } else {
+        finalHour = correctHour
+    }
+    let lettersForHours = null
     if (correctHour < 12) {
         lettersForHours = 'AM'
     } else {
         lettersForHours = 'PM'
     }
-    let checkDay = null
+    let finalDay = null
     let correctMonth = null
     if (correctDay === 0) {
         correctMonth = month -1
         if (correctMonth === 1 || 3 || 5 || 7 || 8 || 10 || 0) {
-            checkDay = 31
+            finalDay = 31
         } else if (correctMonth === 2) {
             if (correctMonth % 4 === 0) {
-                checkDay = 29
+                finalDay = 29
             } else {
-                checkDay = 28
+                finalDay = 28
             }
         } else {
-            checkDay = 30
+            finalDay = 30
         }
     } else {
         correctMonth = month
-        checkDay = correctDay
+        finalDay = correctDay
     }
-    console.log(`CORRECTMONTH: ${correctMonth}`)
-    let checkMonth = null
-    let correctYear = null
+    let finalNumeralMonth = null
+    let finalYear = null
     if (correctMonth === 0) {
-        checkMonth = 12
-        correctYear = year - 1
+        finalNumeralMonth = 12
+        finalYear = year - 1
     } else {
-        checkMonth = correctMonth
-        correctYear = year
+        finalNumeralMonth = correctMonth
+        finalYear = year
     }
-    console.log(`CHECKMONTH: ${checkMonth}`)
-    let spelledMonth = ''
-    if (checkMonth === 1) {
-        spelledMonth = 'January'
-    } else if (checkMonth === 2) {
-        spelledMonth = 'February'
-    } else if (checkMonth === 3) {
-        spelledMonth = 'March'
-    } else if (checkMonth === 4) {
-        spelledMonth = 'April'
-    } else if (checkMonth === 5) {
-        spelledMonth = 'May'
-    } else if (checkMonth === 6) {
-        spelledMonth = 'June'
-    } else if (checkMonth === 7) {
-        spelledMonth = 'July'
-    } else if (checkMonth === 8) {
-        spelledMonth = 'August'
-    } else if (checkMonth === 9) {
-        spelledMonth = 'September'
-    } else if (checkMonth === 10) {
-        spelledMonth = 'October'
-    } else if (checkMonth === 11) {
-        spelledMonth = 'November'
-    } else if (checkMonth === 12) {
-        spelledMonth = 'December'
+    let finalMonth = null
+    if (finalNumeralMonth === 1) {
+        finalMonth = 'January'
+    } else if (finalNumeralMonth === 2) {
+        finalMonth = 'February'
+    } else if (finalNumeralMonth === 3) {
+        finalMonth = 'March'
+    } else if (finalNumeralMonth === 4) {
+        finalMonth = 'April'
+    } else if (finalNumeralMonth === 5) {
+        finalMonth = 'May'
+    } else if (finalNumeralMonth === 6) {
+        finalMonth = 'June'
+    } else if (finalNumeralMonth === 7) {
+        finalMonth = 'July'
+    } else if (finalNumeralMonth === 8) {
+        finalMonth = 'August'
+    } else if (finalNumeralMonth === 9) {
+        finalMonth = 'September'
+    } else if (finalNumeralMonth === 10) {
+        finalMonth = 'October'
+    } else if (finalNumeralMonth === 11) {
+        finalMonth = 'November'
+    } else if (finalNumeralMonth === 12) {
+        finalMonth = 'December'
     }
-    console.log(`SPELLEDMONTH: ${spelledMonth}`)
-    const fullDate = `${meridiemHour}:${minute}${lettersForHours} on ${spelledMonth} ${checkDay}, ${correctYear}`
-    return fullDate
+    const finalDate = `${finalHour}:${finalMinute}${lettersForHours} on ${finalMonth} ${finalDay}, ${finalYear}`
+    return finalDate
 }
 
 export default convertTime
