8d788b5b1610f5dba93d397ce71a0abfc13e707b
jtreeves <jackson.t.reeves@gmail.com>
2021-02-07 14:54:22 -0500

    [Card] Add alt field for images

--- src/components/elements/Card.js
+++ src/components/elements/Card.js
@@ -7,6 +7,7 @@ function Card(props) {
                 <img 
                     className="card-img-top" 
                     src={props.image}
+                    alt={props.title}
                 />
             }
             <div 
