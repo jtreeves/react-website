83192be894d8ea45d6764c9dd7068798b98633f4
jtreeves <jackson.t.reeves@gmail.com>
2021-02-07 13:44:32 -0500

    [Tumblr] Delete unused createElement auto-import

--- src/components/contents/Tumblr.js
+++ src/components/contents/Tumblr.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, createElement } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Card from '../elements/Card'
 import convertTime from '../../utilities/time'
