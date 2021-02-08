61d6667d3eb9de2fccaca9e3d7eae0e19ea6bea2
jtreeves <jackson.t.reeves@gmail.com>
2021-02-08 00:20:42 -0500

    [Footer] Add social media icons below copyright

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -1,6 +1,39 @@
 function Footer() {
     return (
-        <footer>© 2021 Jackson Reeves</footer>
+        <footer>
+            <p>
+                © 2021 Jackson Reeves
+            </p>
+            <ul className="navigation">
+                <li>
+                    <img 
+                        src="/images/social/github.png" 
+                        alt="GitHub"
+                    />
+                </li>
+                
+                <li>
+                    <img 
+                        src="/images/social/linkedin.png" 
+                        alt="LinkedIn"
+                    />
+                </li>
+                
+                <li>
+                    <img 
+                        src="/images/social/twitter.png" 
+                        alt="Twitter"
+                    />
+                </li>
+                
+                <li>
+                    <img 
+                        src="/images/social/tumblr.png" 
+                        alt="Tumblr"
+                    />
+                </li>
+            </ul>
+        </footer>
     )
 }
 
