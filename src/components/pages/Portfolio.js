3a54a6e4e053e7733aabac8c56a1a54994b91c29
jtreeves <jackson.t.reeves@gmail.com>
2021-02-07 19:18:17 -0500

    [Portfolio] Restyle with two columns

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -14,75 +14,79 @@ function Portfolio() {
                 description={description}
             />
 
-            <Card
-                image={projects.regressions.image}
-                title={projects.regressions.name}
-                danger={false}
-                text={projects.regressions.description}
-                source={false}
-                cloud={projects.regressions.skills}
-                link={projects.regressions.deployedLink}
-                button={projects.regressions.type}
-                otherLink={projects.regressions.repositoryLink}
-                otherButton="Repo"
-                time={projects.regressions.date}
-            />
-            
-            <Card
-                image={projects.kaleidoscope.image}
-                title={projects.kaleidoscope.name}
-                danger={false}
-                text={projects.kaleidoscope.description}
-                source={false}
-                cloud={projects.kaleidoscope.skills}
-                link={projects.kaleidoscope.deployedLink}
-                button={projects.kaleidoscope.type}
-                otherLink={projects.kaleidoscope.repositoryLink}
-                otherButton="Repo"
-                time={projects.kaleidoscope.date}
-            />
+            <div className="contains-columns">
+                <div className="left-column">
+                    <Card
+                        image={projects.regressions.image}
+                        title={projects.regressions.name}
+                        danger={false}
+                        text={projects.regressions.description}
+                        source={false}
+                        cloud={projects.regressions.skills}
+                        link={projects.regressions.deployedLink}
+                        button={projects.regressions.type}
+                        otherLink={projects.regressions.repositoryLink}
+                        otherButton="Repo"
+                        time={projects.regressions.date}
+                    />
+                    
+                    <Card
+                        image={projects.tiresias.image}
+                        title={projects.tiresias.name}
+                        danger={false}
+                        text={projects.tiresias.description}
+                        source={false}
+                        cloud={projects.tiresias.skills}
+                        link={projects.tiresias.deployedLink}
+                        button={projects.tiresias.type}
+                        otherLink={projects.tiresias.repositoryLink}
+                        otherButton="Repo"
+                        time={projects.tiresias.date}
+                    />
+                </div>
 
-            <Card
-                image={projects.tiresias.image}
-                title={projects.tiresias.name}
-                danger={false}
-                text={projects.tiresias.description}
-                source={false}
-                cloud={projects.tiresias.skills}
-                link={projects.tiresias.deployedLink}
-                button={projects.tiresias.type}
-                otherLink={projects.tiresias.repositoryLink}
-                otherButton="Repo"
-                time={projects.tiresias.date}
-            />
+                <div className="right-column">
+                    <Card
+                        image={projects.kaleidoscope.image}
+                        title={projects.kaleidoscope.name}
+                        danger={false}
+                        text={projects.kaleidoscope.description}
+                        source={false}
+                        cloud={projects.kaleidoscope.skills}
+                        link={projects.kaleidoscope.deployedLink}
+                        button={projects.kaleidoscope.type}
+                        otherLink={projects.kaleidoscope.repositoryLink}
+                        otherButton="Repo"
+                        time={projects.kaleidoscope.date}
+                    />
 
-            <Card
-                image={projects.matchesAndPatches.image}
-                title={projects.matchesAndPatches.name}
-                danger={false}
-                text={projects.matchesAndPatches.description}
-                source={false}
-                cloud={projects.matchesAndPatches.skills}
-                link={projects.matchesAndPatches.deployedLink}
-                button={projects.matchesAndPatches.type}
-                otherLink={projects.matchesAndPatches.repositoryLink}
-                otherButton="Repo"
-                time={projects.matchesAndPatches.date}
-            />
+                    <Card
+                        image={projects.readThisNext.image}
+                        title={projects.readThisNext.name}
+                        danger={false}
+                        text={projects.readThisNext.description}
+                        source={false}
+                        cloud={projects.readThisNext.skills}
+                        link={projects.readThisNext.deployedLink}
+                        button={projects.readThisNext.type}
+                        otherLink={projects.readThisNext.repositoryLink}
+                        otherButton="Repo"
+                        time={projects.readThisNext.date}
+                    />
+                </div>
+            </div>
 
-            <Card
-                image={projects.readThisNext.image}
-                title={projects.readThisNext.name}
-                danger={false}
-                text={projects.readThisNext.description}
-                source={false}
-                cloud={projects.readThisNext.skills}
-                link={projects.readThisNext.deployedLink}
-                button={projects.readThisNext.type}
-                otherLink={projects.readThisNext.repositoryLink}
-                otherButton="Repo"
-                time={projects.readThisNext.date}
-            />
+            <a 
+                href="https://github.com/jtreeves" 
+                target="_blank" 
+                rel="noreferrer"
+            >
+                <button
+                    className="btn btn-primary center top-margin"
+                >
+                    View more projects
+                </button>
+            </a>
         </div>
     )
 }
