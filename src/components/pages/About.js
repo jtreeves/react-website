4aa55610be59448d3fa4cd5dcdecf31c285f3208
jtreeves <jackson.t.reeves@gmail.com>
2021-02-07 20:20:02 -0500

    [About] Add headshot

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,15 +1,15 @@
-import Introduction from '../sections/Introduction'
-
 function About() {
-    const name = 'About'
-    const description = 'In college, I was a humanities major, and that taught me how to think and how to express myself coherently. As a journalist, I learned how to deliver on deadline and internalize specific style guidelines. As a marketer, I learned how to track website traffic and how to customize specific content for different platforms and clientele. As a tutor, I learned how to guide others and got experience with challenges that arise in one-on-one situations in which one party knows drastically more than the other. Additionally, I think I can explain anything to a ten-year-old, which I think means that I should be able to explain anything that I understand to anyone. Now, I hope to parlay all of those skills to become a programmer.'
-    
     return (
         <div>
-            <Introduction 
-                name={name}
-                description={description}
+            <h2>About</h2>
+            <img 
+                src="/images/headshot.png" 
+                alt="Jackson Reeves" 
+                className="headshot"
             />
+            <p>
+                In college, I was a humanities major, and that taught me how to think and how to express myself coherently. As a journalist, I learned how to deliver on deadline and internalize specific style guidelines. As a marketer, I learned how to track website traffic and how to customize specific content for different platforms and clientele. As a tutor, I learned how to guide others and got experience with challenges that arise in one-on-one situations in which one party knows drastically more than the other. Additionally, I think I can explain anything to a ten-year-old, which I think means that I should be able to explain anything that I understand to anyone. Now, I hope to parlay all of those skills to become a programmer.
+            </p>
         </div>
     )
 }
