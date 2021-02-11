eb7010f63b79a3c738173f2833f9f159d3bdfa03
jtreeves <jackson.t.reeves@gmail.com>
2021-02-11 12:54:06 -0500

    [css] Reduce footer's top margin by using a negative number

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -13,7 +13,7 @@ function About() {
                 </div>
                 
                 <div className="right-column">
-                    <p>
+                    <p className="card">
                         In college, I was a humanities major, and that taught me how to think and how to express myself coherently. As a journalist, I learned how to deliver on deadline and internalize specific style guidelines. As a marketer, I learned how to track website traffic and how to customize specific content for different platforms and clientele. As a tutor, I learned how to guide others and got experience with challenges that arise in one-on-one situations in which one party knows drastically more than the other. Additionally, I think I can explain anything to a ten-year-old, which I think means that I should be able to explain anything that I understand to anyone. Now, I hope to parlay all of those skills to become a programmer.
                     </p>
                 </div>
