60cd775f7d1ac6c906658e6d1fae9afba7dac6f4
jtreeves <jackson.t.reeves@gmail.com>
2021-02-11 12:43:01 -0500

    [css] Use flexbox to center header items vertically

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -1,63 +1,7 @@
 function Footer() {
     return (
         <footer>
-            <p>
                 © 2021 Jackson Reeves
-            </p>
-
-            <ul>
-                <li>
-                    <a 
-                        href="https://github.com/jtreeves" 
-                        target="_blank" 
-                        rel="noreferrer"
-                    >
-                        <img 
-                            src="/images/social/github.png" 
-                            alt="GitHub"
-                        />
-                    </a>
-                </li>
-                
-                <li>
-                    <a 
-                        href="https://www.linkedin.com/in/jacksonreeves/" 
-                        target="_blank" 
-                        rel="noreferrer"
-                    >
-                        <img 
-                            src="/images/social/linkedin.png" 
-                            alt="LinkedIn"
-                        />
-                    </a>
-                </li>
-                
-                <li>
-                    <a 
-                        href="https://twitter.com/JTReeves" 
-                        target="_blank" 
-                        rel="noreferrer"
-                    >
-                        <img 
-                            src="/images/social/twitter.png" 
-                            alt="Twitter"
-                        />
-                    </a>
-                </li>
-                
-                <li>
-                    <a 
-                        href="https://programming10101.tumblr.com" 
-                        target="_blank" 
-                        rel="noreferrer"
-                    >
-                        <img 
-                            src="/images/social/tumblr.png" 
-                            alt="Tumblr"
-                        />
-                    </a>
-                </li>
-            </ul>
         </footer>
     )
 }
--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -4,17 +4,77 @@ import Navigation from './Navigation'
 function Header() {
     return (
         <header>
-            <Link 
-                to="/" 
-                style={{ 
-                    textDecoration: "none", 
-                    color: "black" 
-                }}
-            >
-                <h1>Jackson Reeves</h1> 
-            </Link>
+            <div className="contains-columns">
+                <div className="left-column">
+                    <Link 
+                        to="/" 
+                        style={{ 
+                            textDecoration: "none", 
+                            color: "black",
+                            lineHeight: "100px"
+                        }}
+                    >
+                        <h1>Jackson Reeves</h1> 
+                    </Link>
+                </div>
 
-            <Navigation />
+                <div className="right-column">
+                    <ul>
+                        <li>
+                            <a 
+                                href="https://github.com/jtreeves" 
+                                target="_blank" 
+                                rel="noreferrer"
+                            >
+                                <img 
+                                    src="/images/social/github.png" 
+                                    alt="GitHub"
+                                />
+                            </a>
+                        </li>
+                        
+                        <li>
+                            <a 
+                                href="https://www.linkedin.com/in/jacksonreeves/" 
+                                target="_blank" 
+                                rel="noreferrer"
+                            >
+                                <img 
+                                    src="/images/social/linkedin.png" 
+                                    alt="LinkedIn"
+                                />
+                            </a>
+                        </li>
+                        
+                        <li>
+                            <a 
+                                href="https://twitter.com/JTReeves" 
+                                target="_blank" 
+                                rel="noreferrer"
+                            >
+                                <img 
+                                    src="/images/social/twitter.png" 
+                                    alt="Twitter"
+                                />
+                            </a>
+                        </li>
+                        
+                        <li>
+                            <a 
+                                href="https://programming10101.tumblr.com" 
+                                target="_blank" 
+                                rel="noreferrer"
+                            >
+                                <img 
+                                    src="/images/social/tumblr.png" 
+                                    alt="Tumblr"
+                                />
+                            </a>
+                        </li>
+                    </ul>
+                    <Navigation />
+                </div>
+            </div>
         </header>
     )
 }
