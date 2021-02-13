b5255e9c1d5c26cfcebf7a581b3d56c37eb954d3
jtreeves <jackson.t.reeves@gmail.com>
2021-02-12 22:37:36 -0500

    [SocialIcons] Refactor to store icons portion of header in separate file

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom'
+
+import SocialIcons from './SocialIcons'
 import Navigation from './Navigation'
 
 function Header() {
@@ -19,67 +21,7 @@ function Header() {
                 </div>
 
                 <div className="right-column">
-                    <ul>
-                        <li>
-                            <a 
-                                href="https://github.com/jtreeves" 
-                                target="_blank" 
-                                rel="noreferrer"
-                            >
-                                <i className="fab fa-github-square fa-lg" />
-                            </a>
-                        </li>
-                        
-                        <li>
-                            <a 
-                                href="https://www.linkedin.com/in/jacksonreeves/" 
-                                target="_blank" 
-                                rel="noreferrer"
-                            >
-                                <i className="fab fa-linkedin fa-lg" />
-                            </a>
-                        </li>
-                        
-                        <li>
-                            <a 
-                                href="https://twitter.com/JTReeves" 
-                                target="_blank" 
-                                rel="noreferrer"
-                            >
-                                <i className="fab fa-twitter-square fa-lg" />
-                            </a>
-                        </li>
-                        
-                        <li>
-                            <a 
-                                href="https://programming10101.tumblr.com" 
-                                target="_blank" 
-                                rel="noreferrer"
-                            >
-                                <i className="fab fa-tumblr-square fa-lg" />
-                            </a>
-                        </li>
-                        
-                        <li>
-                            <a 
-                                href="mailto:jr@jacksonreeves.com" 
-                                target="_blank" 
-                                rel="noreferrer"
-                            >
-                                <i className="fas fa-envelope-square fa-lg" />
-                            </a>
-                        </li>
-                        
-                        <li>
-                            <a 
-                                href="/resume.pdf" 
-                                target="_blank" 
-                                rel="noreferrer"
-                            >
-                                <i className="fas fa-external-link-square-alt fa-lg" />
-                            </a>
-                        </li>
-                    </ul>
+                    <SocialIcons />
                     <Navigation />
                 </div>
             </div>
---
+++ src/components/sections/SocialIcons.js
@@ -0,0 +1,67 @@
+function SocialIcons() {
+    return (
+        <ul>
+            <li>
+                <a 
+                    href="https://github.com/jtreeves" 
+                    target="_blank" 
+                    rel="noreferrer"
+                >
+                    <i className="fab fa-github-square fa-lg" />
+                </a>
+            </li>
+            
+            <li>
+                <a 
+                    href="https://www.linkedin.com/in/jacksonreeves/" 
+                    target="_blank" 
+                    rel="noreferrer"
+                >
+                    <i className="fab fa-linkedin fa-lg" />
+                </a>
+            </li>
+            
+            <li>
+                <a 
+                    href="https://twitter.com/JTReeves" 
+                    target="_blank" 
+                    rel="noreferrer"
+                >
+                    <i className="fab fa-twitter-square fa-lg" />
+                </a>
+            </li>
+            
+            <li>
+                <a 
+                    href="https://programming10101.tumblr.com" 
+                    target="_blank" 
+                    rel="noreferrer"
+                >
+                    <i className="fab fa-tumblr-square fa-lg" />
+                </a>
+            </li>
+            
+            <li>
+                <a 
+                    href="mailto:jr@jacksonreeves.com" 
+                    target="_blank" 
+                    rel="noreferrer"
+                >
+                    <i className="fas fa-envelope-square fa-lg" />
+                </a>
+            </li>
+            
+            <li>
+                <a 
+                    href="/resume.pdf" 
+                    target="_blank" 
+                    rel="noreferrer"
+                >
+                    <i className="fas fa-external-link-square-alt fa-lg" />
+                </a>
+            </li>
+        </ul>
+    )
+}
+
+export default SocialIcons
