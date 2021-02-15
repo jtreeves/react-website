92d0921cde48fad7eacd4f1b3bcd869e608ad96f
jtreeves <jackson.t.reeves@gmail.com>
2021-02-15 18:45:22 -0500

    [Project] Re-add hover after moving contains-columns class to top div

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -14,66 +14,79 @@ function Project() {
     }
 
     return (
-        <div>
+        <div className="contains-columns">
             <h2>{specificProject}</h2>
 
-            <div className="contains-columns">
-                <div className="left-column">
-                    <div className="square">
-                        <img 
-                            src={projects[lowercase].image} 
-                            alt={projects[lowercase].name} 
-                            className="square-content card"
-                        />    
-                    </div>
-                </div>
+            <div className="left-column">
+                <a 
+                    href={projects[lowercase].deployedLink} 
+                    target="_blank" 
+                    rel="noreferrer"
+                    className="links"
+                >
+                    <div className="project-highlight square">
+                        <div className="project-image">
+                            <img 
+                                src={projects[lowercase].image} 
+                                alt={projects[lowercase].name} 
+                                className="square-content card"
+                            />
+                        </div>
 
-                <div className="right-column">
-                    <div className="square">
-                        <div className="square-content card">
-                            <div className="card-header">
-                                <h4>
-                                    About This Project
-                                </h4>
+                        <div className="square-content card project-overlay">
+                            <div className="project-title">
+                                View Site
                             </div>
+                        </div>
+                    </div>            
+                </a>
+            </div>
 
-                            <div className="card-body">
-                                {projects[lowercase].description}
-                                <SkillSet 
-                                    skills={projects[lowercase].skills}
-                                />
-                            </div>
+            <div className="right-column">
+                <div className="square">
+                    <div className="square-content card">
+                        <div className="card-header">
+                            <h4>
+                                About This Project
+                            </h4>
+                        </div>
 
-                            <div className="card-footer">
-                                <a 
-                                    href={projects[lowercase].deployedLink} 
-                                    target="_blank" 
-                                    rel="noreferrer"
+                        <div className="card-body">
+                            {projects[lowercase].description}
+                            <SkillSet 
+                                skills={projects[lowercase].skills}
+                            />
+                        </div>
+
+                        <div className="card-footer">
+                            <a 
+                                href={projects[lowercase].deployedLink} 
+                                target="_blank" 
+                                rel="noreferrer"
+                            >
+                                <button 
+                                    className="btn btn-primary float-left"
                                 >
-                                    <button 
-                                        className="btn btn-primary float-left"
-                                    >
-                                        {projects[lowercase].type}
-                                    </button>
-                                </a>
-                    
-                                <a 
-                                    href={projects[lowercase].repositoryLink} 
-                                    target="_blank" 
-                                    rel="noreferrer"
+                                    {projects[lowercase].type}
+                                </button>
+                            </a>
+                
+                            <a 
+                                href={projects[lowercase].repositoryLink} 
+                                target="_blank" 
+                                rel="noreferrer"
+                            >
+                                <button 
+                                    className="btn btn-primary float-left other-button"
                                 >
-                                    <button 
-                                        className="btn btn-primary float-left other-button"
-                                    >
-                                        Code
-                                    </button>
-                                </a>
-                    
-                                <div 
-                                    className="float-right text-muted card-footer-connect"
-                                >
-                                    {projects[lowercase].date}
-                                </div>
+                                    Code
+                                </button>
+                            </a>
+                
+                            <div 
+                                className="float-right text-muted card-footer-connect"
+                            >
+                                {projects[lowercase].date}
                             </div>
                         </div>
                     </div>
