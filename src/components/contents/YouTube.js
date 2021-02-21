1abaaac1537040150570c378f4ddb98002d54b83
jtreeves <jackson.t.reeves@gmail.com>
2021-02-21 01:53:27 -0500

    [YouTube] Use iframe tag to display video

--- src/components/contents/YouTube.js
+++ src/components/contents/YouTube.js
@@ -1,11 +1,9 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-import Card from '../elements/Card'
-
-import convertTime from '../../utilities/time'
-
 const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL
+
+const youtubeUrl = 'https://www.youtube.com/embed/'
 
 function YouTube() {
     const [videos, setVideos] = useState([])
@@ -15,28 +13,22 @@ function YouTube() {
             const result = await axios.get(
                 REACT_APP_SERVER_URL + '/youtube'
             )
-            const resultArray = result.data.videos.map((video, index) => {
-                const date = convertTime(video.published_timestamp)
+            const resultArray = result.data.videos.items.map((video, index) => {
+                const youtubeId = video.id.videoId
+                const sourceLink = youtubeUrl + youtubeId
                 if (index < 5) {
                     return (
                         <div 
                             key={index}
                         >
-                            <Card 
-                                image={video.cover_image}
-                                title={video.title}
-                                subtitle={false}
-                                lead={false}
-                                danger={false}
-                                list={false}
-                                text={video.description}
-                                source={false}
-                                cloud={false}
-                                link={video.url}
-                                button="View full video"
-                                otherLink={false}
-                                otherButton={false}
-                                time={date}
+                            <iframe 
+                                width="560" 
+                                height="315" 
+                                src={sourceLink}
+                                title={index}
+                                frameborder="0" 
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                                allowfullscreen 
                             />
                         </div>
                     )
