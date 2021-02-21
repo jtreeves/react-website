ed7e7890b5027abae18a0dbf5fbe5453dfe9fa91
jtreeves <jackson.t.reeves@gmail.com>
2021-02-21 03:48:34 -0500

    [SocialIcons] Add links for DEV, Medium, and YouTube channels

--- src/components/sections/SocialIcons.js
+++ src/components/sections/SocialIcons.js
@@ -38,11 +38,41 @@ function SocialIcons() {
             
             <li>
                 <a 
+                    href="https://dev.to/jtreeves" 
+                    target="_blank" 
+                    rel="noreferrer"
+                >
+                    <i className="fab fa-dev fa-2x" />
+                </a>
+            </li>
+            
+            <li>
+                <a 
+                    href="https://jtreeves.medium.com" 
+                    target="_blank" 
+                    rel="noreferrer"
+                >
+                    <i className="fab fa-medium fa-2x" />
+                </a>
+            </li>
+            
+            <li>
+                <a 
                     href="https://programming10101.tumblr.com" 
                     target="_blank" 
                     rel="noreferrer"
                 >
                     <i className="fab fa-tumblr-square fa-2x" />
+                </a>
+            </li>
+            
+            <li>
+                <a 
+                    href="https://www.youtube.com/c/jacksonreeves" 
+                    target="_blank" 
+                    rel="noreferrer"
+                >
+                    <i className="fab fa-youtube-square fa-2x" />
                 </a>
             </li>
             
