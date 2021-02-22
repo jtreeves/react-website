d44799953e0b86d5bf86bf7aa9e54793be12569d
jtreeves <jackson.t.reeves@gmail.com>
2021-02-22 12:58:50 -0500

    [RecentProjects] Create component for rendering recent projects on landing page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,17 +2,13 @@ import Portfolio from './Portfolio'
 import Contact from './Contact'
 import AllSkills from './AllSkills'
 import Introduction from '../sections/Introduction'
+import RecentProjects from '../sections/RecentProjects'
 
 function Home() {
     return (
         <div>
             <Introduction />
-            <hr />
-            <Portfolio />
-            <hr />
-            <AllSkills />
-            <hr />
-            <Contact />
+            {/* <RecentProjects /> */}
         </div>
     )
 }
---
+++ src/components/sections/RecentProjects.js
@@ -0,0 +1,63 @@
+import ProjectHighlight from '../sections/ProjectHighlight'
+
+import projects from '../../database/projects'
+
+function RecentProjects() {
+    return (
+        <div className="recent-projects">
+            <div className="recent-projects-intro">
+                <div className="recent-projects-intro-left">
+                    <hr />
+                </div>
+
+                <div className="recent-projects-intro-center">
+                    <h2>Recent Projects</h2>
+                </div>
+
+                <div className="recent-projects-intro-right">
+                    <hr />
+                </div>
+            </div>
+
+            <div className="recent-projects-list">
+                <ProjectHighlight 
+                    image={projects.regressions.image}
+                    name={projects.regressions.name}
+                    description={projects.regressions.description}
+                    skills={projects.regressions.skills}
+                    deployedLink={projects.regressions.deployedLink}
+                    deployedText={projects.regressions.type}
+                    repositoryLink={projects.regressions.repositoryLink}
+                    repositoryText="Code"
+                    date={projects.regressions.date}
+                />
+
+                <ProjectHighlight 
+                    image={projects.kaleidoscope.image}
+                    name={projects.kaleidoscope.name}
+                    description={projects.kaleidoscope.description}
+                    skills={projects.kaleidoscope.skills}
+                    deployedLink={projects.kaleidoscope.deployedLink}
+                    deployedText={projects.kaleidoscope.type}
+                    repositoryLink={projects.kaleidoscope.repositoryLink}
+                    repositoryText="Code"
+                    date={projects.kaleidoscope.date}
+                />
+
+                <ProjectHighlight 
+                    image={projects.tiresias.image}
+                    name={projects.tiresias.name}
+                    description={projects.tiresias.description}
+                    skills={projects.tiresias.skills}
+                    deployedLink={projects.tiresias.deployedLink}
+                    deployedText={projects.tiresias.type}
+                    repositoryLink={projects.tiresias.repositoryLink}
+                    repositoryText="Code"
+                    date={projects.tiresias.date}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default RecentProjects
