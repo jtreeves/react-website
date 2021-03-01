5455ad503e1dc8264d75ab98bf079f0e806c9ab2
jtreeves <jackson.t.reeves@gmail.com>
2021-02-28 21:59:48 -0500

    [About] Edit text

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -28,22 +28,22 @@ function About() {
                                 body="In college, I was a humanities major, and that taught me how to think and how to express myself coherently. As a journalist, I learned how to deliver on deadline and internalize specific style guidelines. As a marketer, I learned how to track website traffic and how to customize specific content for different platforms and clientele. As a tutor, I learned how to guide others and got experience with challenges that arise in one-on-one situations in which one party knows drastically more than the other. Additionally, I think I can explain anything to a ten-year-old, which I think means that I should be able to explain anything that I understand to anyone. Now, I hope to parlay all of those skills to become a programmer."
                             />
 
-                            <FavoritesCard 
-                                header="Favorite Movies"
-                                elements={[
-                                    {
-                                        title: "The Social Network",
-                                        creator: "David Fincher"
-                                    }, 
-                                    {
-                                        title: "Star Wars", 
-                                        creator: "George Lucas"
-                                    }, 
-                                    {
-                                        title: "The Big Sleep", 
-                                        creator: "Howard Hawks"
-                                    }
-                                ]}
+                            <QuoteCard 
+                                header="Movie Quote"
+                                text="The time to make up your mind about people... is never."
+                                source="The Philadelphia Story"
+                            />
+                        </div>
+
+                        <div className="one-column-of-three">
+                            <TextCard 
+                                header="Recent"
+                                body="After completing an intensive, four-month program in software engineering from General Assembly, I'm excited to branch out into a new career in web development and programming. I hope to use the skills I've honed from working in math and editing to produce clean, performative code."
+                            />
+
+                            <ImageCard 
+                                title="Reading List"
+                                image="/images/books.jpg"
                             />
 
                             <TestimonialsTeaser 
@@ -52,35 +52,17 @@ function About() {
                                 source={testimonials.nicole.name}
                                 company={testimonials.nicole.company}
                             />
-                        </div>
-
-                        <div className="one-column-of-three">
-                            <TextCard 
-                                header="Recent"
-                                body="A digital native living in the South, Jackson Reeves is a jack-of-all-trades with experience in the education, journalism, and digital media industries. He currently works as the director of Hyperion Academics. As a professional nerd, he teaches students algebra, geometry, and calculus; helps them improve their study skills; and coaches them to ace the entire battery of standardized tests, including the SAT and ACT. He previously served as the digital media producer at Atlanta magazine, one of the country's first city publications. There, he managed the magazine's social media endeavors and newsletter programs, updated the website with editorial content from the print edition each month, and wrote for the Daily Agenda and Covered Dish blogs."
-                            />
-
-                            <ImageCard 
-                                title="Reading List"
-                                image="/images/books.jpg"
-                            />
-
-                            <QuoteCard 
-                                header="Movie Quote"
-                                text="The time to make up your mind about people... is never."
-                                source="The Philadelphia Story"
-                            />
                             
                             <FavoritesCard 
                                 header="Favorite Books"
                                 elements={[
                                     {
-                                        title: "War and Peace",
-                                        creator: "Leo Tolstoy"
+                                        title: "A Visit from the Goon Squad",
+                                        creator: "Jennifer Egan"
                                     }, 
                                     {
-                                        title: "Foundation and Empire", 
-                                        creator: "Isaac Asimov"
+                                        title: "A Wild Sheep Chase", 
+                                        creator: "Haruki Murakami"
                                     }, 
                                     {
                                         title: "Endless Night", 
@@ -89,6 +71,7 @@ function About() {
                                 ]}
                             />
                         </div>
+
                         <div className="one-column-of-three">
                             <QuoteCard 
                                 header="Programming Quote"
@@ -98,7 +81,25 @@ function About() {
 
                             <TextCard 
                                 header="Passions"
-                                body="Prior to joining Atlanta, Reeves plugged away at various local and national newspapers and magazines and received a degree in media studies with minors in English and philosophy from Vassar College. The Sandy Springs native returned to his hometown in 2009 to escape the hassles of poststructuralist theory, immerse himself in Southern hospitality, and binge on pork belly. In his spare time, he enjoys reading the literature of Bret Easton Ellis and Anne Rivers Siddons, listening to Soundgarden and Sneaker Pimps, and watching anything written by Tina Fey. That, or getting outside and playing with his dog, Tucker. And he likes the Internet. A lot. If interested in reaching Reeves for freelance work, feel free to send him an email. His areas of expertise include education, social media, copyediting, and data analysis."
+                                body="In my spare time, I like to solve calculus problems from one of the many math workbooks I keep in case of emergencies. Emergencies like not having enough practice sets to do. Ever since I was a young child, I have been trying to find the perfect outlet for my analytical orientation. In the past, that has included using a spreadsheet to organize my library chronologically. Now, it includes playing with SQL and building algorithms."
+                            />
+
+                            <FavoritesCard 
+                                header="Favorite Movies"
+                                elements={[
+                                    {
+                                        title: "The Social Network",
+                                        creator: "David Fincher"
+                                    }, 
+                                    {
+                                        title: "O Brother, Where Art Thou?", 
+                                        creator: "Joel and Ethan Coen"
+                                    }, 
+                                    {
+                                        title: "Indiana Jones and the Raiders of the Lost Ark", 
+                                        creator: "Steven Spielberg"
+                                    }
+                                ]}
                             />
 
                             <ImageCard 
@@ -106,7 +107,6 @@ function About() {
                                 image="/images/hike.jpg"
                             />
                         </div>
-                        
                     </div>
                 </main>
             </div>
