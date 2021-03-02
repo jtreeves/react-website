4efa08e683db56139069b2edcecd025de523a61a
jtreeves <jackson.t.reeves@gmail.com>
2021-03-02 15:53:44 -0500

    [Resume] Adjust for multiple screen sizes

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -14,6 +14,118 @@ import '../../style/skills.css'
 import '../../style/resume.css'
 
 function Resume() {
+    const skillsHeading = <SectionHeading 
+        heading="Skills" 
+    />
+
+    const educationHeading = <SectionHeading 
+        heading="Education" 
+    />
+
+    const experienceHeading = <SectionHeading 
+        heading="Experience" 
+    />
+
+    const testimonialHeading = <SectionHeading 
+        heading="Testimonial" 
+    />
+
+    const languages = <SkillCard 
+        description="Languages"
+        category={types.languages}
+    />
+
+    const frameworks = <SkillCard 
+        description="Frameworks"
+        category={types.frameworks}
+    />
+
+    const libraries = <SkillCard 
+        description="Libraries"
+        category={types.libraries}
+    />
+
+    const databases = <SkillCard 
+        description="Databases"
+        category={types.databases}
+    />
+
+    const tools = <SkillCard 
+        description="Tools"
+        category={types.tools}
+    />
+
+    const methodologies = <SkillCard 
+        description="Methodologies"
+        category={types.methodologies}
+    />
+
+    const certificate = <SchoolCard 
+        school={education.certificate.school}
+        degree={education.certificate.degree}
+        specialization={education.certificate.specialization}
+        elements={education.certificate.courses}
+        date={education.certificate.date}
+        link={education.certificate.link}
+    />
+
+    const bachelor = <SchoolCard 
+        school={education.bachelor.school}
+        degree={education.bachelor.degree}
+        specialization={education.bachelor.specialization}
+        elements={education.bachelor.courses}
+        date={education.bachelor.date}
+        link={education.bachelor.link}
+    />
+
+    const program = <SchoolCard 
+        school={education.program.school}
+        degree={education.program.degree}
+        specialization={education.program.specialization}
+        elements={education.program.courses}
+        date={education.program.date}
+        link={education.program.link}
+    />
+
+    const hyperionDirector = <CompanyCard 
+        company={experience.hyperionDirector.company}
+        position={experience.hyperionDirector.position}
+        responsibilities={experience.hyperionDirector.items}
+        date={experience.hyperionDirector.date}
+        link={experience.hyperionDirector.link}
+    />
+
+    const tutorSenior = <CompanyCard 
+        company={experience.tutorSenior.company}
+        position={experience.tutorSenior.position}
+        responsibilities={experience.tutorSenior.items}
+        date={experience.tutorSenior.date}
+        link={experience.tutorSenior.link}
+    />
+
+    const magazineDigital = <CompanyCard 
+        company={experience.magazineDigital.company}
+        position={experience.magazineDigital.position}
+        responsibilities={experience.magazineDigital.items}
+        date={experience.magazineDigital.date}
+        link={experience.magazineDigital.link}
+    />
+
+    const magazineEditor = <CompanyCard 
+        company={experience.magazineEditor.company}
+        position={experience.magazineEditor.position}
+        responsibilities={experience.magazineEditor.items}
+        date={experience.magazineEditor.date}
+        link={experience.magazineEditor.link}
+    />
+
+    const testimonial = <TestimonialsTeaser 
+        header={testimonials.nicole.callout}
+        text={testimonials.nicole.quote}
+        source={testimonials.nicole.name}
+        company={testimonials.nicole.company}
+    />
+
     return (
         <div>
             <PageOpener 
@@ -23,125 +135,147 @@ function Resume() {
             
             <div class="overcoming-header">
                 <main>
-                    <div className="uneven-columns">
+                    <div 
+                        className="uneven-columns"
+                        id="resume-full"
+                    >
                         <div>
-                            <SectionHeading heading="Skills" />
+                            {skillsHeading}
 
                             <div className="single-column">
-                                <SkillCard 
-                                    description="Languages"
-                                    category={types.languages}
-                                />
-                                
-                                <SkillCard 
-                                    description="Frameworks"
-                                    category={types.frameworks}
-                                />
-                                
-                                <SkillCard 
-                                    description="Libraries"
-                                    category={types.libraries}
-                                />
-                                
-                                <SkillCard 
-                                    description="Databases"
-                                    category={types.databases}
-                                />
-                                
-                                <SkillCard 
-                                    description="Tools"
-                                    category={types.tools}
-                                />
-                                
-                                <SkillCard 
-                                    description="Methodologies"
-                                    category={types.methodologies}
-                                />
+                                {languages}
+                                {frameworks}
+                                {libraries}
+                                {databases}
+                                {tools}
+                                {methodologies}
                             </div>
 
                             <div className="blank-space" />
 
-                            <SectionHeading heading="Testimonial" />
+                            {testimonialHeading}
 
                             <div className="single-column">
-                                <TestimonialsTeaser 
-                                    header={testimonials.nicole.callout}
-                                    text={testimonials.nicole.quote}
-                                    source={testimonials.nicole.name}
-                                    company={testimonials.nicole.company}
-                                />
+                                {testimonial}
                             </div>
                         </div>
 
                         <div>
-                            <SectionHeading heading="Education" />
+                            {educationHeading}
 
                             <div className="single-column">
-                                <SchoolCard 
-                                    school={education.certificate.school}
-                                    degree={education.certificate.degree}
-                                    specialization={education.certificate.specialization}
-                                    elements={education.certificate.courses}
-                                    date={education.certificate.date}
-                                    link={education.certificate.link}
-                                />
-                                
-                                <SchoolCard 
-                                    school={education.bachelor.school}
-                                    degree={education.bachelor.degree}
-                                    specialization={education.bachelor.specialization}
-                                    elements={education.bachelor.courses}
-                                    date={education.bachelor.date}
-                                    link={education.bachelor.link}
-                                />
-                                
-                                <SchoolCard 
-                                    school={education.program.school}
-                                    degree={education.program.degree}
-                                    specialization={education.program.specialization}
-                                    elements={education.program.courses}
-                                    date={education.program.date}
-                                    link={education.program.link}
-                                />
+                                {certificate}
+                                {bachelor}
+                                {program}
                             </div>
 
                             <div className="blank-space" />
 
-                            <SectionHeading heading="Experience" />
+                            {experienceHeading}
 
                             <div className="single-column">
-                                <CompanyCard 
-                                    company={experience.hyperionDirector.company}
-                                    position={experience.hyperionDirector.position}
-                                    responsibilities={experience.hyperionDirector.items}
-                                    date={experience.hyperionDirector.date}
-                                    link={experience.hyperionDirector.link}
-                                />
-                                
-                                <CompanyCard 
-                                    company={experience.tutorSenior.company}
-                                    position={experience.tutorSenior.position}
-                                    responsibilities={experience.tutorSenior.items}
-                                    date={experience.tutorSenior.date}
-                                    link={experience.tutorSenior.link}
-                                />
-                                
-                                <CompanyCard 
-                                    company={experience.magazineDigital.company}
-                                    position={experience.magazineDigital.position}
-                                    responsibilities={experience.magazineDigital.items}
-                                    date={experience.magazineDigital.date}
-                                    link={experience.magazineDigital.link}
-                                />
-                                
-                                <CompanyCard 
-                                    company={experience.magazineEditor.company}
-                                    position={experience.magazineEditor.position}
-                                    responsibilities={experience.magazineEditor.items}
-                                    date={experience.magazineEditor.date}
-                                    link={experience.magazineEditor.link}
-                                />
+                                {hyperionDirector}
+                                {tutorSenior}
+                                {magazineDigital}
+                                {magazineEditor}
                             </div>
+                        </div>
+                    </div> 
+                    
+                    <div 
+                        id="resume-limited"
+                    >
+                        {skillsHeading}
+
+                        <div className="two-columns">
+                            <div className="single-column">
+                                {languages}
+                                {methodologies}
+                            </div>
+
+                            <div className="single-column">
+                                {frameworks}
+                                {libraries}
+                                {databases}
+                                {tools}
+                            </div>
+                        </div>
+
+                        <div className="blank-space" />
+
+                        <div className="two-columns">
+                            <div>
+                                {educationHeading}
+                                
+                                <div className="single-column">
+                                    {certificate}
+                                    {bachelor}
+                                    {program}
+                                </div>
+
+                                <div className="blank-space" />
+                                
+                                {testimonialHeading}
+                                
+                                <div className="single-column">    
+                                    {testimonial}
+                                </div>
+                            </div>
+
+                            <div>
+                                {experienceHeading}
+
+                                <div className="single-column">
+                                    {hyperionDirector}
+                                    {tutorSenior}
+                                    {magazineDigital}
+                                    {magazineEditor}
+                                </div>
+                            </div>
+                        </div>
+                    </div> 
+                    
+                    <div 
+                        id="resume-single"
+                    >
+                        {skillsHeading}
+
+                        <div className="single-column">
+                            {languages}
+                            {frameworks}
+                            {libraries}
+                            {databases}
+                            {tools}
+                            {methodologies}
+                        </div>
+
+                        <div className="blank-space" />
+
+                        {educationHeading}
+
+                        <div className="single-column">
+                            {certificate}
+                            {bachelor}
+                            {program}
+                        </div>
+
+                        <div className="blank-space" />
+
+                        {experienceHeading}
+                        
+                        <div className="single-column">
+                            {hyperionDirector}
+                            {tutorSenior}
+                            {magazineDigital}
+                            {magazineEditor}
+                        </div>
+
+                        <div className="blank-space" />
+                        
+                        {testimonialHeading}
+                        
+                        <div className="single-column">
+                            {testimonial}
                         </div>
                     </div> 
 
