dfb3ad092e1367b737e8bef69109320e39d281a5
jtreeves <jackson.t.reeves@gmail.com>
2021-03-02 10:51:37 -0500

    [About] Make page adapt to different screen sizes

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -7,7 +7,68 @@ import testimonials from '../../database/testimonials'
 import TestimonialsTeaser from '../elements/TestimonialsTeaser'
 import { textCards, listCards, quoteCards, imageCards } from '../../database/about'
 
+import '../../style/about.css'
+
 function About() {
+    const workingImage = <ImageCard 
+        title={imageCards.working.title}
+        image={imageCards.working.image}
+    />
+
+    const recentText = <TextCard 
+        header={textCards.recent.header}
+        body={textCards.recent.body}
+    />
+        
+    const programmingQuote = <QuoteCard 
+        header={quoteCards.programming.header}
+        text={quoteCards.programming.text}
+        source={quoteCards.programming.source}
+    />
+
+    const backgroundText = <TextCard 
+        header={textCards.background.header}
+        body={textCards.background.body}
+    />
+
+    const booksImage = <ImageCard 
+        title={imageCards.books.title}
+        image={imageCards.books.image}
+    />
+
+    const passionsText = <TextCard 
+        header={textCards.passions.header}
+        body={textCards.passions.body}
+    />
+
+    const simoneTestimonial = <TestimonialsTeaser 
+        header={testimonials.simone.callout}
+        text={testimonials.simone.quote}
+        source={testimonials.simone.name}
+        company={testimonials.simone.company}
+    />
+
+    const movieQuote = <QuoteCard 
+        header={quoteCards.movie.header}
+        text={quoteCards.movie.text}
+        source={quoteCards.movie.source}
+    />
+
+    const booksList = <ListCard 
+        header={listCards.books.header}
+        elements={listCards.books.elements}
+    />
+
+    const moviesList = <ListCard 
+        header={listCards.movies.header}
+        elements={listCards.movies.elements}
+    />
+
+    const hikingImage = <ImageCard 
+        title={imageCards.hiking.title}
+        image={imageCards.hiking.image}
+    />
+
     return (
         <div>
             <PageOpener 
@@ -17,71 +78,68 @@ function About() {
             
             <div class="overcoming-header">
                 <main>
-                    <div className="three-columns">
+                    <div 
+                        className="three-columns" 
+                        id="about-three"
+                    >
                         <div className="single-column">
-                            <ImageCard 
-                                title={imageCards.working.title}
-                                image={imageCards.working.image}
-                            />
-
-                            <TextCard 
-                                header={textCards.background.header}
-                                body={textCards.background.body}
-                            />
-
-                            <QuoteCard 
-                                header={quoteCards.movie.header}
-                                text={quoteCards.movie.text}
-                                source={quoteCards.movie.source}
-                            />
+                            {workingImage}
+                            {backgroundText}
+                            {movieQuote}
                         </div>
 
                         <div className="single-column">
-                            <TextCard 
-                                header={textCards.recent.header}
-                                body={textCards.recent.body}
-                            />
+                            {recentText}
+                            {booksImage}
+                            {simoneTestimonial}
+                            {booksList}
+                        </div>
+                        
+                        <div className="single-column">
+                            {programmingQuote}
+                            {passionsText}
+                            {moviesList}
+                            {hikingImage}
+                        </div>
+                    </div>
 
-                            <ImageCard 
-                                title={imageCards.books.title}
-                                image={imageCards.books.image}
-                            />
-
-                            <TestimonialsTeaser 
-                                header={testimonials.simone.callout}
-                                text={testimonials.simone.quote}
-                                source={testimonials.simone.name}
-                                company={testimonials.simone.company}
-                            />
-                            
-                            <ListCard 
-                                header={listCards.books.header}
-                                elements={listCards.books.elements}
-                            />
+                    <div 
+                        className="two-columns"
+                        id="about-two"
+                    >
+                        <div className="single-column">
+                            {workingImage}
+                            {backgroundText}
+                            {simoneTestimonial}
+                            {moviesList}
+                            {hikingImage}
                         </div>
 
                         <div className="single-column">
-                            <QuoteCard 
-                                header={quoteCards.programming.header}
-                                text={quoteCards.programming.text}
-                                source={quoteCards.programming.source}
-                            />
-
-                            <TextCard 
-                                header={textCards.passions.header}
-                                body={textCards.passions.body}
-                            />
-
-                            <ListCard 
-                                header={listCards.movies.header}
-                                elements={listCards.movies.elements}
-                            />
-
-                            <ImageCard 
-                                title={imageCards.hiking.title}
-                                image={imageCards.hiking.image}
-                            />
+                            {recentText}
+                            {programmingQuote}
+                            {booksImage}
+                            {passionsText}
+                            {booksList}
+                            {movieQuote}
                         </div>
+                    </div>
+
+                    <div 
+                        className="single-column"
+                        id="about-one"
+                    >
+                        {recentText}
+                        {workingImage}
+                        {backgroundText}
+                        {programmingQuote}
+                        {booksImage}
+                        {passionsText}
+                        {simoneTestimonial}
+                        {hikingImage}
+                        {movieQuote}
+                        {booksList}
+                        {moviesList}
                     </div>
                 </main>
             </div>
