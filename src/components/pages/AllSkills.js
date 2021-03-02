13134d6bf514c4f479f49463f8a7ed6da90a968f
jtreeves <jackson.t.reeves@gmail.com>
2021-03-02 14:33:50 -0500

    [AllSkills] Make cards responsive across screen sizes

--- src/components/pages/AllSkills.js
+++ src/components/pages/AllSkills.js
@@ -5,6 +5,36 @@ import types from '../../database/types'
 import '../../style/skills.css'
 
 function AllSkills() {
+    const languages = <SkillCard 
+        description="Languages"
+        category={types.languages}
+    />
+
+    const frameworks = <SkillCard 
+        description="Frameworks"
+        category={types.frameworks}
+    />
+
+    const libraries = <SkillCard 
+        description="Libraries"
+        category={types.libraries}
+    />
+
+    const databases = <SkillCard 
+        description="Databases"
+        category={types.databases}
+    />
+
+    const tools = <SkillCard 
+        description="Tools"
+        category={types.tools}
+    />
+
+    const methodologies = <SkillCard 
+        description="Methodologies"
+        category={types.methodologies}
+    />
+
     return (
         <div>
             <PageOpener 
@@ -14,42 +44,53 @@ function AllSkills() {
             
             <div class="overcoming-header">
                 <main>
-                    <div className="three-columns">
+                    <div 
+                        className="three-columns"
+                        id="skills-three"
+                    >
                         <div className="single-column">
-                            <SkillCard 
-                                description="Languages"
-                                category={types.languages}
-                            />
-                            
-                            <SkillCard 
-                                description="Libraries"
-                                category={types.libraries}
-                            />
+                            {languages}
+                            {libraries}
                         </div>
 
                         <div className="single-column">
-                            <SkillCard 
-                                description="Methodologies"
-                                category={types.methodologies}
-                            />
+                            {methodologies}
                         </div>
 
                         <div className="single-column">
-                            <SkillCard 
-                                description="Frameworks"
-                                category={types.frameworks}
-                            />
-                            
-                            <SkillCard 
-                                description="Databases"
-                                category={types.databases}
-                            />
-                            
-                            <SkillCard 
-                                description="Tools"
-                                category={types.tools}
-                            />
+                            {frameworks}
+                            {databases}
+                            {tools}
                         </div>
+                    </div>
+
+                    <div 
+                        className="two-columns"
+                        id="skills-two"
+                    >
+                        <div className="single-column">
+                            {languages}
+                            {methodologies}
+                        </div>
+
+                        <div className="single-column">
+                            {frameworks}
+                            {libraries}
+                            {databases}
+                            {tools}
+                        </div>
+                    </div>
+                    
+                    <div 
+                        className="single-column"
+                        id="skills-one"
+                    >
+                        {languages}
+                        {frameworks}
+                        {libraries}
+                        {databases}
+                        {tools}
+                        {methodologies}
                     </div>
                 </main>
             </div>
