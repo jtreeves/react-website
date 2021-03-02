ec0d7f34163f681aced4258bc0b1ab919105e633
jtreeves <jackson.t.reeves@gmail.com>
2021-03-01 21:15:06 -0500

    [css] Create separate classes for the nav icons for the intro section

--- src/components/sections/Introduction.js
+++ src/components/sections/Introduction.js
@@ -15,49 +15,49 @@ function Introduction() {
 
             <div className="introduction-columns">
                 <div className="introduction-columns-first">
-                    <div className="floater-icon-nav">
+                    <div className="floater-icon-nav-intro">
                         <Link 
                             to="/skills"
                             onClick={scroll}
                             className="floater-links"
                         >
-                            <div className="floater-icon">
+                            <div className="floater-icon-intro">
                                 <i class="fas fa-laptop-code fa-5x" />
                             </div>
 
-                            <div className="floater-text">
+                            <div className="floater-text-intro">
                                 Skills
                             </div>
                         </Link>
                     </div>
 
-                    <div className="floater-icon-nav">
+                    <div className="floater-icon-nav-intro">
                         <Link 
                             to="/resume"
                             onClick={scroll}
                             className="floater-links"
                         >
-                            <div className="floater-icon">
+                            <div className="floater-icon-intro">
                                 <i class="fas fa-file-alt fa-5x" />
                             </div>
 
-                            <div className="floater-text">
+                            <div className="floater-text-intro">
                                 Resume
                             </div>
                         </Link>
                     </div>
 
-                    <div className="floater-icon-nav">
+                    <div className="floater-icon-nav-intro">
                         <Link 
                             to="/portfolio"
                             onClick={scroll}
                             className="floater-links"
                         >
-                            <div className="floater-icon">
+                            <div className="floater-icon-intro">
                                 <i class="fas fa-briefcase fa-5x" />
                             </div>
 
-                            <div className="floater-text">
+                            <div className="floater-text-intro">
                                 Portfolio
                             </div>
                         </Link>
@@ -79,49 +79,49 @@ function Introduction() {
                 </div>
 
                 <div className="introduction-columns-last">
-                    <div className="floater-icon-nav">
+                    <div className="floater-icon-nav-intro">
                         <Link 
                             to="/posts"
                             onClick={scroll}
                             className="floater-links"
                         >
-                            <div className="floater-icon">
+                            <div className="floater-icon-intro">
                                 <i class="fas fa-bullhorn fa-5x" />
                             </div>
 
-                            <div className="floater-text">
+                            <div className="floater-text-intro">
                                 Posts
                             </div>
                         </Link>
                     </div>
 
-                    <div className="floater-icon-nav">
+                    <div className="floater-icon-nav-intro">
                         <Link 
                             to="/about"
                             onClick={scroll}
                             className="floater-links"
                         >
-                            <div className="floater-icon">
+                            <div className="floater-icon-intro">
                                 <i class="fas fa-comment fa-5x" />
                             </div>
 
-                            <div className="floater-text">
+                            <div className="floater-text-intro">
                                 About
                             </div>
                         </Link>
                     </div>
 
-                    <div className="floater-icon-nav">
+                    <div className="floater-icon-nav-intro">
                         <Link 
                             to="/contact"
                             onClick={scroll}
                             className="floater-links"
                         >
-                            <div className="floater-icon">
+                            <div className="floater-icon-intro">
                                 <i class="fas fa-paper-plane fa-5x" />
                             </div>
 
-                            <div className="floater-text">
+                            <div className="floater-text-intro">
                                 Contact
                             </div>
                         </Link>
